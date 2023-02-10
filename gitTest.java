95111c220005e18a49d5e4c14e2f85c81f8a9ecc
neverGiveUppppp <csy6219@naver.com>
2023-02-10 16:03:04 +0900

    conflict resolve practice : sourceTree _ test branch

--- gitTest.java
+++ gitTest.java
@@ -2,6 +2,6 @@ public static void main(String args[]){
  Test test = new Test();
  test.conflict();
  test.resolve();
- test.addTestBranch(); // test branch
+ test.addTestBranch(); // test branch feat. sourcetree
 }
 
