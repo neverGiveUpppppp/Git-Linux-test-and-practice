9e16d05a43601c9865d9de1014eeea7fe6ba583d
neverGiveUppppp <csy6219@naver.com>
2023-02-14 13:46:44 +0900

    merge confilct test

--- gitTest.java
+++ gitTest.java
@@ -5,12 +5,13 @@ public static void main(String args[]){
  test.sourcetree();
  Resolve Using 'Mine'
  Resolve Using 'Theirs'
+
  test branch // test branch
+ 
 
- main branch // main branch
 
- //
- //
+
+
 
 }
 
