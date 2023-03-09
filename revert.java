aae894fa5fb78286913fc55abf6c471b966c18ad
neverGiveUppppp <csy6219@naver.com>
2023-03-09 17:11:18 +0900

    git revert ready

--- revert.java
+++ revert.java
@@ -5,3 +5,9 @@ c 구조체
 다른 데이터타입 종류를  묶은 변수 모임
 struck
 
+정적페이지(Static pages) : DB, bussiness logic 필요없이 서버 처리 없이 사용자에게 보여줄 수 있는 페이지
+동적페이지(Dynamic pages) : DB, busssiness logic 등  서버 처리 결과를 포함한 파일을 보여주는 페이지
+
+
+
+
